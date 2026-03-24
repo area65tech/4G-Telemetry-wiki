f221131f209bc403f44b835a6f97cd5bbb1b00f7
hansen khornelius <hansen.khornelius@gmail.com>
2026-03-24 14:07:47 +0800

    custom theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,3 +33,16 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+# -- Custom CSS Configuration 
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+    'css/custom.css',
+]
